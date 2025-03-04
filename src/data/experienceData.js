b32216c3058dc38a41a918b2c3ee042a3a0389e3
Anoop A <65398934+anoop-ashok@users.noreply.github.com>
2025-03-04 08:22:34 +0100

    Update experienceData

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,20 +1,27 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Tata Consultancy Services',
-        jobtitle: 'Technical Lead',
-        startYear: '03/2016',
+        company: 'Bosch',
+        jobtitle: 'Senior Software Engineer',
+        startYear: '06/2023',
         endYear: 'Present'
     },
     {
         id: 2,
+        company: 'Tata Consultancy Services',
+        jobtitle: 'Technical Lead',
+        startYear: '03/2016',
+        endYear: '06/2023'
+    },
+    {
+        id: 3,
         company: 'Cognizant Technology Solutions',
         jobtitle: 'Senior Frontend Developer',
         startYear: '02/2015',
         endYear: '03/2016'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Neologix Software Solutions',
         jobtitle: 'Frontend Developer',
         startYear: '07/2012',
